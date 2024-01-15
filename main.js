d10c5692e1fae2bb7076905ef7e0a736c858ebe0
askld135 <w3218822@navere.com>
2024-01-15 16:17:11 +0900

    <a>부분에 </a>가 없어서 본문에 링크가 걸리던 문제 해결, 기존의 메인페이지에 undefinded가 출력되던 것을 조건문을 중첩하여 환영메세지가 출력되도록 함

--- main.js
+++ main.js
@@ -5,34 +5,59 @@ var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-    var title = queryData.id;
-    
     if(pathname === '/'){
-        fs.readFile(`data/${queryData.id}`,'utf8',function(err, description){
-            var template = `
-            <!DOCTYPE html>
-            <html>
-                <head>
-                    <title>WEB - ${title}</title>
-                    <meta charset = "utf-8">
-                </head>
-            
-                <body>
-                    <h1><a href="/">WEB</a></h1>
-                    <ol>
-                        <li><a href="/?id=html">HTML</li>
-                        <li><a href="/?id=CSS">CSS</li>
-                        <li><a href="/?id=JavaScript">JavaScript</li>
-                    </ol>
-            
-                    <h2>${title}</h2>
-                    <p>${description}</p>
-            </html>`
-            response.writeHead(200);
-            response.end(template); //(querydata.id)를 페이지에 출력하는 역할
-        });
+        if(queryData.id === undefined){
+                var title = 'Welcome';
+                var description = 'Hello, Node.js';
+                var template = `
+                <!DOCTYPE html>
+                <html>
+                    <head>
+                        <title>WEB - ${title}</title>
+                        <meta charset = "utf-8">
+                    </head>
+                
+                    <body>
+                        <h1><a href="/">WEB</a></h1>
+                        <ol>
+                            <li><a href="/?id=html">HTML</a></li>
+                            <li><a href="/?id=CSS">CSS</a></li>
+                            <li><a href="/?id=JavaScript">JavaScript</a></li>
+                        </ol>
+                
+                        <h2>${title}</h2>
+                        <p>${description}</p>
+                </html>`
+                response.writeHead(200);    //웹브라우저가 웹서버에 접근할때, 웹 서버가 브라우저에게 200을 전송하면 파일을 성공적으로 전송했다.
+                response.end(template); //(querydata.id)를 페이지에 출력하는 역할
+        }else{
+            fs.readFile(`data/${queryData.id}`,'utf8',function(err, description){
+                var title = queryData.id;
+                var template = `
+                <!DOCTYPE html>
+                <html>
+                    <head>
+                        <title>WEB - ${title}</title>
+                        <meta charset = "utf-8">
+                    </head>
+                
+                    <body>
+                        <h1><a href="/">WEB</a></h1>
+                        <ol>
+                            <li><a href="/?id=html">HTML</a></li>
+                            <li><a href="/?id=CSS">CSS</a></li>
+                            <li><a href="/?id=JavaScript">JavaScript</a></li>
+                        </ol>
+                
+                        <h2>${title}</h2>
+                        <p>${description}</p>
+                </html>`
+                response.writeHead(200);    //웹브라우저가 웹서버에 접근할때, 웹 서버가 브라우저에게 200을 전송하면 파일을 성공적으로 전송했다.
+                response.end(template); //(querydata.id)를 페이지에 출력하는 역할
+            });
+        }
     }else{
-        response.writeHead(404);
+        response.writeHead(404);    //파일을 찾을수 없는 경우 서버는 404를 전송함
         response.end('Not found');
     }
 });
