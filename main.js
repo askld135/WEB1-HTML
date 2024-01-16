2c7344516da891fb84a12ff7df8cde9c722c50e0
askld135 <w3218822@navere.com>
2024-01-16 15:44:54 +0900

    파일이 추가됨에 따라 자동적으로 추가된 파일이 페이지에 리스트에 추가되어 표시되도록 만들기

--- main.js
+++ main.js
@@ -7,8 +7,24 @@ var app = http.createServer(function(request,response){
     var pathname = url.parse(_url, true).pathname;
     if(pathname === '/'){
         if(queryData.id === undefined){
+            fs.readdir('./data', function(error, filelist){
+                console.log(filelist);
                 var title = 'Welcome';
                 var description = 'Hello, Node.js';
+                /*
+                var list = `<ol>
+                            <li><a href="/?id=html">HTML</a></li>
+                            <li><a href="/?id=CSS">CSS</a></li>
+                            <li><a href="/?id=JavaScript">JavaScript</a></li>
+                        </ol>`;
+                */
+               var list = '<ul>';
+               var i = 0;
+               while(i<filelist.length){
+                list = list + `<li><a href = "/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+                i++;
+               }
+               list = list + '</ul>';
                 var template = `
                 <!DOCTYPE html>
                 <html>
@@ -19,42 +35,42 @@ var app = http.createServer(function(request,response){
                 
                     <body>
                         <h1><a href="/">WEB</a></h1>
-                        <ol>
-                            <li><a href="/?id=html">HTML</a></li>
-                            <li><a href="/?id=CSS">CSS</a></li>
-                            <li><a href="/?id=JavaScript">JavaScript</a></li>
-                        </ol>
-                
+                        ${list}
                         <h2>${title}</h2>
                         <p>${description}</p>
                 </html>`
-                response.writeHead(200);    //웹브라우저가 웹서버에 접근할때, 웹 서버가 브라우저에게 200을 전송하면 파일을 성공적으로 전송했다.
-                response.end(template); //(querydata.id)를 페이지에 출력하는 역할
+                response.writeHead(200);
+                response.end(template);
+            })
         }else{
-            fs.readFile(`data/${queryData.id}`,'utf8',function(err, description){
-                var title = queryData.id;
-                var template = `
-                <!DOCTYPE html>
-                <html>
-                    <head>
-                        <title>WEB - ${title}</title>
-                        <meta charset = "utf-8">
-                    </head>
-                
-                    <body>
-                        <h1><a href="/">WEB</a></h1>
-                        <ol>
-                            <li><a href="/?id=html">HTML</a></li>
-                            <li><a href="/?id=CSS">CSS</a></li>
-                            <li><a href="/?id=JavaScript">JavaScript</a></li>
-                        </ol>
-                
-                        <h2>${title}</h2>
-                        <p>${description}</p>
-                </html>`
-                response.writeHead(200);    //웹브라우저가 웹서버에 접근할때, 웹 서버가 브라우저에게 200을 전송하면 파일을 성공적으로 전송했다.
-                response.end(template); //(querydata.id)를 페이지에 출력하는 역할
-            });
+            fs.readdir('./data', function(error, filelist){
+                fs.readFile(`data/${queryData.id}`,'utf8',function(err, description){
+                    var title = queryData.id;
+                    var list = '<ul>';
+                    var i = 0;
+                    while(i<filelist.length){
+                        list = list + `<li><a href = "/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+                        i++;
+                    }
+                    list = list + '</ul>';
+                    var template = `
+                    <!DOCTYPE html>
+                    <html>
+                        <head>
+                            <title>WEB - ${title}</title>
+                            <meta charset = "utf-8">
+                        </head>
+                    
+                        <body>
+                            <h1><a href="/">WEB</a></h1>
+                            ${list}
+                            <h2>${title}</h2>
+                            <p>${description}</p>
+                    </html>`
+                    response.writeHead(200);    //웹브라우저가 웹서버에 접근할때, 웹 서버가 브라우저에게 200을 전송하면 파일을 성공적으로 전송했다.
+                    response.end(template); //(querydata.id)를 페이지에 출력하는 역할
+                });
+            })
         }
     }else{
         response.writeHead(404);    //파일을 찾을수 없는 경우 서버는 404를 전송함
