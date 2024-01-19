a936d076cc6b252749f13e5b9fce3cef467b584d
askld135 <w3218822@navere.com>
2024-01-19 16:07:38 +0900

    delete 기능을 구현

--- main.js
+++ main.js
@@ -55,7 +55,12 @@ var app = http.createServer(function(request,response){
             var list = templateList(filelist);
             var template = templateHTML(title, list,
               `<h2>${title}</h2>${description}`,
-              `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+              `<a href="/create">create</a>
+               <a href="/update?id=${title}">update</a>
+               <form action="/delete_process" method="post">
+                 <input type="hidden" name="id" value="${title}">
+                 <input type="submit" value="delete"> 
+               </form>`  //querystring을 이용한 get방식을 통해 delete를 구현하면, cashing을 이용한 플러그인으로 인하여 의도하지 않게 삭제가 될 수도 있음.
             );
             response.writeHead(200);
             response.end(template);
@@ -134,6 +139,19 @@ var app = http.createServer(function(request,response){
               response.end();
             })
           });
+      });
+    } else if(pathname === '/delete_process'){
+      var body = '';
+      request.on('data', function(data){
+          body = body + data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+          var id = post.id;
+          fs.unlink(`data/${id}`, function(error){
+            response.writeHead(302, {Location: `/`});
+            response.end();
+          })
       });
     } else {
       response.writeHead(404);
